b48278047a21f76a28c3895236da705753022eaf
ecourse <smkang@codenuri.co.kr>
2025-07-14 13:45:39 +0900

    Add files via upload

--- DAY1/03_jthread3.cpp
+++ DAY1/03_jthread3.cpp
@@ -6,6 +6,13 @@ using namespace std::literals;
 // 새로운 스레드가 "취소" 되도록 하려면
 // flag 변수를 사용해야 합니다.
 
+// 아래 코드를 C# 에서 "cooperative cancelation(협력적 취소)" 라고합니다
+// 그런데, 아래 코드는
+// 스레드가 1개 라서 => main 에서 한개 bool 이면 됩니다.
+// 스레드가 10개 라면 => main 에서 10개 bool 변수 필요
+// => 결국 스레드당 한개 필요!!
+// => jthread 가 이기술을 지원
+
 void foo( bool& quit_signal )
 {
 	for ( int i = 0; i < 10; i++ )
