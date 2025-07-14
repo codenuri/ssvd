d3e9e9ba0b47896054fac1b2dbbd92c67ae4b309
ecourse <smkang@codenuri.co.kr>
2025-07-14 11:45:37 +0900

    Add files via upload

--- DAY1/02_thread9-1.cpp
+++ DAY1/02_thread9-1.cpp
@@ -19,11 +19,33 @@ void init()
 }
 //------------------------------------------------------------
 
+// 단일 스레드로 구간의 합을 구하기
 template<typename T>
 void sum(T first, T last, int& result)
 {
     result = std::accumulate(first, last, result);
 }
+
+// 다중 스레드로 구간의 합을 구하기.
+template<typename T>
+void parallel_sum(T first, T last, int& result)
+{
+    // #1. 요소의 갯수 파악
+//	const unsigned long cnt_element = last - first; // vector, 배열등의 연속된 메모리는 가능
+													// list 등의 반복자라면 error
+
+	const unsigned long cnt_element = std::dist(first, last); // ok. 좋은 코드!!
+
+	if ( cnt_element == 0 ) return 0;
+
+	
+
+}
+
+
+
+
+
 
 int main()
 {
