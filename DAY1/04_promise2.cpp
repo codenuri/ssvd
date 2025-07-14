6e1c0181be3382079a5ff965d82f7de88dd73182
ecourse <smkang@codenuri.co.kr>
2025-07-14 14:27:59 +0900

    Add files via upload

--- DAY1/04_promise2.cpp
+++ DAY1/04_promise2.cpp
@@ -5,8 +5,12 @@
 using namespace std::literals;
 
 // Promise & future 모델 : 스레드로 부터 결과값(과 예외)를 꺼내오기 위한 C++의 표준 방법
+// => 특징, pro.set_value() 는 1회만 호출 가능합니다.
+// => 즉, 결과 한개를 꺼낼때만 사용
+// => 루프안에서 주기적으로 결과를 전달하려면 "condion_variable" 사용 - 내일 배우는 내용.
 
-void add(int a, int b, std::promise<int>& pro )
+//void add(int a, int b, std::promise<int>& pro )
+void add(int a, int b, std::promise<int>&& pro )
 {
 	int s = a + b;
 	std::this_thread::sleep_for(3s);
@@ -26,7 +30,11 @@ int main()
 	std::future<int> ft = pro.get_future();
 
 	// #2. promise 객체를 참조나 move 로 전달합니다.
-	std::thread t(add, 10, 20, std::ref(pro)); 
+//	std::thread t(add, 10, 20, std::ref(pro)); // 이렇게 해도 되지만
+	std::thread t(add, 10, 20, std::move(pro)); // 이렇게도 많이 합니다
+												// 현재 코드에서 promise 객체는 이제 main 에서는
+												// 필요 없다는 의미로 move 로 전달
+												// 단, 이경우 받을때는 promise<int>&&
 
 
 
