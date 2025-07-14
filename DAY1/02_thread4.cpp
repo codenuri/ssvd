69ef284ecbccebe48a3cd77ce78be2fee69e7209
ecourse <smkang@codenuri.co.kr>
2025-07-14 10:30:11 +0900

    Add files via upload

--- DAY1/02_thread4.cpp
+++ DAY1/02_thread4.cpp
@@ -15,10 +15,12 @@ int main()
     
     std::this_thread::sleep_for(1s);
 
-	std::thread::id tid1 = std::this_thread::get_id();
-    std::thread::id tid2 = t.get_id();
+	// 이번 소스 핵심은 아래 2줄의 차이점입니다.
+	std::thread::id tid1 = std::this_thread::get_id(); // 현재 스레드(주스레드) ID
+    std::thread::id tid2 = t.get_id();				   // 새롭게 생성된 스레드의 스레드 ID
     
-    std::cout << "created thread id : " << tid << std::endl;
+    std::cout << "main    thread id : " << tid1 << std::endl;
+	std::cout << "created thread id : " << tid2 << std::endl;
 
     t.join();
 }
