eedcf8728cee1799d4dfd51ed1a8738545f4b102
ecourse <smkang@codenuri.co.kr>
2025-07-14 10:09:01 +0900

    Add files via upload

--- DAY1/02_thread2.cpp
+++ DAY1/02_thread2.cpp
@@ -8,6 +8,9 @@ void f1() { }
 void f2(int a, double d) { }
 void f3(int a, int& b, std::string&& s) { b = 100; }
 
+// std::string&& : C++11 의 std::move 관련 기술 - 별도의 학습이 필요합니다.
+// => 잘 모르시는 분은 그냥 reference 로 이해 하고 수업 들어셔도 됩니다.
+// => 정확한 명칭은 "rvalue reference" 입니다.
 int main()
 {
     int n = 0;
@@ -27,6 +30,9 @@ int main()
 	// => 하지만, 필요한 경우가 있습니다. 오후에 등장.. 
 
 }
+// 본과정 복습을 위해서 "동영상강의"가 제공됩니다.
+// => 수강을 희망하시면 "ID" 한개만 만들어서 운영자에게 알려주세요
+// => 개인 정보와 관련 없는 ID 로 해주세요.
 
 
 
