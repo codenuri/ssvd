2d6b8485d003a56e8373eeee52328e18468e9872
ecourse <smkang@codenuri.co.kr>
2025-07-14 11:37:56 +0900

    Add files via upload

--- DAY1/02_thread9.cpp
+++ DAY1/02_thread9.cpp
@@ -7,7 +7,7 @@
 
 // 핵심 : 지금 까지 배운 내용으로 만드는 "예제"
 
-constexpr std::size_t sz = 1_000_000;
+constexpr std::size_t sz = 1'000'000;
 
 std::vector<int> v;
 
@@ -23,6 +23,8 @@ void init()
 // 구간의 합을 구하는 함수.
 void sum(int first, int last, int& result)
 {
+	// std::accumulate : 주어진 구간의 합을 구하는 C++ 표준 함수
+	//                   마지막 인자는 합을 구할때 사용하는 sum 변수의 초기값(보통 0)
     result = std::accumulate(first, last, result);
 }
 
@@ -31,6 +33,8 @@ int main()
 	init();
 
     int s = 0;
-    sum(v.begin(), v.end(), s);
+    
+	sum(v.begin(), v.end(), s);
+
     std::cout << s << std::endl;
 }
