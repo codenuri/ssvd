2ff893cac011d483a8cfb7b8479e0476f276c0a5
ecourse <smkang@codenuri.co.kr>
2025-07-14 15:48:21 +0900

    Add files via upload

--- DAY1/07_async3.cpp
+++ DAY1/07_async3.cpp
@@ -8,23 +8,29 @@ using namespace std::literals;
 
 int add(int a, int b)
 {
-    std::cout << "start add" << std::endl;
+    std::cout << "A. start add" << std::endl; 
     std::this_thread::sleep_for(2s);
-    std::cout << "finish add" << std::endl;
+    std::cout << "B. finish add" << std::endl; 
     return a + b;
 }
 
 int main()
 {
-	// #1. 아래 코드 실행해서 결과(화면 로그)를 예측해 보세요
-	// => continue main 결과
-    std::future<int> ft = std::async( std::launch::async, add, 10, 20);
+	// #1. 아래 코드 실행 결과(화면 로그)를 예측해 보세요
+	// => 1. A, C 출력 => 순서는 알수 없음
+	// => 2. B
+	// => 3. D 
+//  std::future<int> ft = std::async( std::launch::async, add, 10, 20);
 
-    std::cout << "continue main " << std::endl;
 
-    int ret = ft.get();
+	// #2. 아래 처럼 std::async 의 반환 값을 받지 않으면 실행결과가 이상해 집니다.
+	std::async( std::launch::async, add, 10, 20);
 
-    std::cout << "finish main" << std::endl;
+    std::cout << "C. continue main " << std::endl; 
+
+//  int ret = ft.get();
+
+    std::cout << "D. finish main" << std::endl;	
 }
 
 
