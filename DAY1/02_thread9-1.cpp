72f7d940ecafe2d9a20c6846fef46688da5a5abf
ecourse <smkang@codenuri.co.kr>
2025-07-14 11:54:58 +0900

    Add files via upload

--- DAY1/02_thread9-1.cpp
+++ DAY1/02_thread9-1.cpp
@@ -34,11 +34,28 @@ void parallel_sum(T first, T last, int& result)
 //	const unsigned long cnt_element = last - first; // vector, 배열등의 연속된 메모리는 가능
 													// list 등의 반복자라면 error
 
-	const unsigned long cnt_element = std::dist(first, last); // ok. 좋은 코드!!
+	const unsigned long cnt_element = std::distance(first, last); // ok. 좋은 코드!!
 
-	if ( cnt_element == 0 ) return 0;
+	if ( cnt_element == 0 ) return;
 
-	
+	// #2. 요소의 갯수로 스레드갯수 결정
+	const unsigned long cnt_per_thread = 25; // 한개당 최소 25개 수행
+	const unsigned long max_thread = (cnt_element + cnt_per_thread -1) / cnt_per_thread;
+					// => 요소가 90개라면 4개 스레드 필요
+					// => 요소가 900개 라면 40개 스레드 필요
+
+
+	// #3. 현재 PC 의 코어의 갯수를 구합니다.
+	unsigned long hw_thread = std::thread::hardware_concurrency();
+
+	if ( hw_thread == 0) // 혹시 위 함수의 구현이 제공되지 않으면
+		hw_thread = 2;   // 최대 2개
+
+	// #4. 이제 실제 필요한 스레드의 수를 계산합니다.
+	// => 결국, 스레드 프로그램의 핵심중의 하나는 "몇개" 를 만들것인가!! 중요합니다.
+	const unsigned long cnt_thread = std::min(hw_thread, max_thread);
+
+	std::cout << " 필요한 스레드 갯수 : " << cnt_thread << std::endl;
 
 }
 
@@ -53,7 +70,8 @@ int main()
 
     int s = 0;
     
-	sum(v.begin(), v.end(), s);
+	//sum(v.begin(), v.end(), s);
+	parallel_sum(v.begin(), v.end(), s);
 
     std::cout << s << std::endl;
 }
