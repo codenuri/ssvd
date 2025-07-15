3451685fe56451615d9b6f43ae32c27f0a5cf97d
ecourse <smkang@codenuri.co.kr>
2025-07-15 11:47:32 +0900

    Add files via upload

--- DAY2/08_condition5.cpp
+++ DAY2/08_condition5.cpp
@@ -11,6 +11,11 @@ using namespace std::literals;
 std::shared_mutex m;	
 int share_data = 0;
 
+// 생산후에만 꺼내 갈수 있도록
+std::condition_variable cv;
+bool data_ready = false;
+
+
 void Writer()
 {
     while (1)
@@ -21,7 +26,11 @@ void Writer()
 			share_data = share_data + 1;
 			std::cout << "Writer : " << share_data << std::endl;
 			std::this_thread::sleep_for(1s);        	
+
+			data_ready = true;
 		}
+		cv.notify_all(); // 대기중인 모든 스레드를 깨우기. 
+
         std::this_thread::sleep_for(10ms);
     }
 }
@@ -31,7 +40,11 @@ void Reader(std::string_view name)
     while (1)
     {
 		{
-			std::shared_lock<std::shared_mutex> g(m); 
+//			std::shared_lock<std::shared_mutex> g(m); 
+			std::unique_lock<std::shared_mutex> g(m); 
+
+			cv.wait(g, [](){ return data_ready;});
+
 			std::cout << "Reader(" << name << ") : " << share_data << std::endl;
 			std::this_thread::sleep_for(500ms);
 		}
