71972604d51e22e8151b0db68d68bd05a3520981
ecourse <smkang@codenuri.co.kr>
2025-07-15 09:14:44 +0900

    Add files via upload

---
+++ DAY2/02_shared_mutex.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <shared_mutex>
+#include <string_view>
+using namespace std::literals;
+
+// shared_mutex : 중요한 개념
+
+std::mutex m;
+int share_data = 0;
+
+// 공유 데이타에 계속 쓰는 작업을 하는 함수
+void Writer()
+{
+    while (1)
+    {
+        m.lock();
+        share_data = share_data + 1;
+        std::cout << "Writer : " << share_data << std::endl;
+        std::this_thread::sleep_for(1s);
+        m.unlock();
+
+        std::this_thread::sleep_for(10ms);
+    }
+}
+// 공유 자원을 읽기만 하는 함수. 
+void Reader(std::string_view name)
+{
+    while (1)
+    {
+        m.lock();
+        std::cout << "Reader(" << name << ") : " << share_data << std::endl;
+        std::this_thread::sleep_for(500ms);
+        m.unlock();
+
+        std::this_thread::sleep_for(10ms);
+    }
+}
+
+int main()
+{
+	// 핵심 : 공유자원에 쓰는 스레드는 1개 
+	//		  공유자원을 읽는 스레드는 3개 
+    std::thread t1(Writer);
+    std::thread t2(Reader, "A");
+	std::thread t3(Reader, "B");
+	std::thread t4(Reader, "C");
+    t1.join();
+    t2.join();
+	t3.join();
+	t4.join();
+}
+
+
+
