37f9bdb37afddfc76dd79745bcbcbaa5af12739d
ecourse <smkang@codenuri.co.kr>
2025-07-15 14:24:35 +0900

    Add files via upload

--- DAY2/10_thread_pool5.cpp
+++ DAY2/10_thread_pool5.cpp
@@ -6,6 +6,7 @@
 #include <condition_variable>
 #include <chrono>
 #include <future>
+#include <functional>
 using namespace std::literals;
 
 int add(int a, int b)
@@ -18,11 +19,12 @@ int main()
 	// packaged_task 객체 사용하기
 
 	std::packaged_task<int(int, int)> task1(&add);
+
 	std::packaged_task<int()> task2(std::bind(&add, 1, 2));
 
 	task1(1, 2);
 
 	task2();
-	
+
 	
 }
