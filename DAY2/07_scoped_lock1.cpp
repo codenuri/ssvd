85cacb37223fb6efc1d75a313c0cac0822ad2710
ecourse <smkang@codenuri.co.kr>
2025-07-15 10:45:00 +0900

    Add files via upload

--- DAY2/07_scoped_lock1.cpp
+++ DAY2/07_scoped_lock1.cpp
@@ -14,7 +14,12 @@ struct  Account
 void transfer(Account& acc1, Account& acc2, int cnt)
 {
 	// 계좌 접근시 mutex 를 획득후 사용합니다.
-	
+	// => 2개 이상의 뮤텍스를 획득할때 아래 처럼하면
+	// => "deadlock" 가능성이 있습니다. 아래 상황..
+
+	// => thread A : acc1.m 을 획득후 acc2.m.lock() 시도
+	// => thread B : acc2.m 을 획득후 acc1.m.lock() 시도
+	// => 해결책 : 다음소스
 	acc1.m.lock();
 	acc2.m.lock();
 
@@ -26,7 +31,6 @@ void transfer(Account& acc1, Account& acc2, int cnt)
 
     std::cout << "finish transfer" << std::endl;
 }
-
 
 int main()
 {
