e97bd06b059dfe4b61a632bbbc6810fa9f598e9a
ecourse <smkang@codenuri.co.kr>
2025-07-15 11:51:23 +0900

    Add files via upload

--- DAY2/08_condition5.cpp
+++ DAY2/08_condition5.cpp
@@ -4,6 +4,7 @@
 #include <mutex>
 #include <shared_mutex>
 #include <string_view>
+#include <condition_variable>
 using namespace std::literals;
 
 // 생산자 1개, 소비자 3개인 모델
@@ -12,7 +13,12 @@ std::shared_mutex m;
 int share_data = 0;
 
 // 생산후에만 꺼내 갈수 있도록
-std::condition_variable cv;
+//std::condition_variable cv;	// 반드시 std::unique_lock 과 같이 사용해야 합니다.
+							// cv.wait() 시에 lock()/unlock() 사용
+							// shared_mutex 의 lock_shared 기능 사용안됩니다.
+	
+std::condition_variable_any cv; // shared_lock 등도 사용가능.
+
 bool data_ready = false;
 
 
@@ -40,10 +46,9 @@ void Reader(std::string_view name)
     while (1)
     {
 		{
-//			std::shared_lock<std::shared_mutex> g(m); 
-			std::unique_lock<std::shared_mutex> g(m); 
-
-			cv.wait(g, [](){ return data_ready;});
+			
+			std::shared_lock<std::shared_mutex> g(m); 
+			cv.wait(g, [](){ return data_ready;} );
 
 			std::cout << "Reader(" << name << ") : " << share_data << std::endl;
 			std::this_thread::sleep_for(500ms);
