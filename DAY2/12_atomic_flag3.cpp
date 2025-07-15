6c597f45b788bc6b18296bfdaac48d736c042648
ecourse <smkang@codenuri.co.kr>
2025-07-15 15:53:40 +0900

    Add files via upload

--- DAY2/12_atomic_flag3.cpp
+++ DAY2/12_atomic_flag3.cpp
@@ -2,6 +2,26 @@
 #include <atomic>
 #include <thread>
 
+/*
+template<typename T>
+class atomic 
+{
+	T value;
+
+	// 모든 구현은 value 가 다양한 타입임을 고려해서 설계 되어야 합니다.
+	// 제약이 있을수 밖에 없습니다.
+};
+
+
+class atomic_flag 
+{
+	// value 가 bool 이라는 확정이 있으므로
+	// 보다 최적화된 설계가 가능합니다.
+	// 모든 C++ 표준 구현은 관련 연산을 lock-free 로 제공해야 합니다.
+};
+*/
+
+
 
 class spinlock
 {
