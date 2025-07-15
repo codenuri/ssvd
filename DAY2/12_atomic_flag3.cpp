fe2e8312410f7d224802837aea116033b43d8065
ecourse <smkang@codenuri.co.kr>
2025-07-15 15:50:46 +0900

    Add files via upload

---
+++ DAY2/12_atomic_flag3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <atomic>
+#include <thread>
+
+
+class spinlock
+{
+	std::atomic_flag flag = ATOMIC_FLAG_INIT; // C++20 부터는 초기화 없어도 됨. 
+public:
+	void lock() { while (flag.test_and_set()); }
+	void unlock() { flag.clear(); }
+};
+spinlock spin;
+
+void work()
+{
+	spin.lock();
+	std::cout << "start. using shared resource" << std::endl;
+	std::cout << "end.   using shared resource" << std::endl;
+	spin.unlock();
+}
+
+int main()
+{
+	std::thread t1(work),t2(work);
+
+	t1.join();
+	t2.join();
+}
