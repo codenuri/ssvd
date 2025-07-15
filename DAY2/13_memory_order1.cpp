93fab11c5a63d53c69314f803ddb29dc91145efa
ecourse <smkang@codenuri.co.kr>
2025-07-15 16:08:16 +0900

    Add files via upload

---
+++ DAY2/13_memory_order1.cpp
@@ -0,0 +1,25 @@
+﻿// 아래 goo() 안의 주석 생각해 보세요
+int a = 0;
+int b = 0;
+
+// thread A
+void foo()
+{
+    a = b + 1;
+    b = 1;
+}
+
+// thread B
+void goo()
+{
+    if (b == 1)
+    {
+        // a == 1 을 보장할수 있을까 ?
+    }
+}
+
+
+
+
+
+
