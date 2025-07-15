7660ed54446196d86899c94e26634b433fb71102
ecourse <smkang@codenuri.co.kr>
2025-07-15 14:24:01 +0900

    Add files via upload

---
+++ DAY2/10_thread_pool5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+#include <future>
+using namespace std::literals;
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
+int main()
+{
+	// packaged_task 객체 사용하기
+
+	std::packaged_task<int(int, int)> task1(&add);
+	std::packaged_task<int()> task2(std::bind(&add, 1, 2));
+
+	task1(1, 2);
+
+	task2();
+	
+	
+}
