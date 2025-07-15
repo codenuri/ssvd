1b34edee4039265938f4bb58847ac3ad52d947bd
ecourse <smkang@codenuri.co.kr>
2025-07-15 13:10:30 +0900

    Add files via upload

--- DAY2/09_producer_consume2.cpp
+++ DAY2/09_producer_consume2.cpp
@@ -4,20 +4,30 @@
 #include <queue>
 #include <mutex>
 #include <condition_variable>
-
+#include <chrono>
+using namespace std::literals;
 
 std::queue<int> Q; 
 std::mutex m;		// Q 의 접근 동기화를 위한 뮤텍스
+
+// 아래 코드는 mutex 로 접근 동기화를 하므로
+// => 동시접근 문제는 해결했습니다.
+
+// 남은 문제는
+// => Q가 비었을때는 소비자는 대기 해야 합니다.
+// => 생산자가 생산후에 소비 해야 합니다.
+
 
 
 void producer()
 {
 	while (true)
 	{
+		static int cnt = 0;
+		
+		std::this_thread::sleep_for(1ms); // 지연
 		{
 			std::lock_guard<std::mutex> g(m);
-
-			static int cnt = 0;
 			Q.push(++cnt);
 			std::cout << "producer : " << cnt << std::endl;
 		}
