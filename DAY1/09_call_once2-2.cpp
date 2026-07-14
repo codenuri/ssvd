65ac8a162f9ed35f51378c3b02aa72acb513f170
ecourse <smkang@codenuri.co.kr>
2026-07-14 17:19:52 +0900

    Add files via upload

--- DAY1/09_call_once2-2.cpp
+++ DAY1/09_call_once2-2.cpp
@@ -14,6 +14,8 @@ public:
 
 	// std::call_once 기술이 아닌 NULL check 기술
 	// => 유명한 기술
+	// case #1. 아래 코드는 싱글 스레드는 오직 한개의 객체를 보장할수 있습니다.
+	//          하지만 멀티 스레드 환경에서는 오직 한개임이 보장 안됩니다.
     static Singleton* getInstance()
     {
 		if ( sinstance == nullptr )
