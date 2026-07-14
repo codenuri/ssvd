238f8404db3c4481c456b2aa358e47fb11e91d01
ecourse <smkang@codenuri.co.kr>
2026-07-14 17:25:39 +0900

    Add files via upload

--- DAY1/09_call_once2-3.cpp
+++ DAY1/09_call_once2-3.cpp
@@ -17,7 +17,14 @@ public:
 	inline static std::mutex mtx;
 
 	// case #2. std::mutex 도입하는 경우
-	// => 
+	// => 멀티 스레드 환경에서도 오직 한개만 생성됨을 보장
+	// 하지만 성능을 생각해 봅시다.
+	// getInstance()를 100번 호출시
+	// => 최초 호출   : 객체 생성
+	// => 나머지 99번 : 생성된 객체를 반환만 합니다.
+	//				   반환만 하면 되는데, mtx.lock(), mtx.unlock()을 거쳐야 합니다.
+	//				   성능이 좋지 않습니다.
+	//				   해결책 : DCLP ????
     static Singleton* getInstance()
     {
 		mtx.lock();
