d330273f0d080ec3faca9b5103d66b7bf14b58be
ecourse <smkang@codenuri.co.kr>
2026-07-14 13:22:02 +0900

    Add files via upload

--- DAY1/03_jthread2.cpp
+++ DAY1/03_jthread2.cpp
@@ -4,6 +4,11 @@
 using namespace std::literals;
 
 // cooperative cancelation(협력적 취소)
+// 아래와 같은 취소가 많이 사용되는 기술 입니다.
+// 그런데, 직접 만들려면
+// => 스레드당 1개의 bool 변수를 만들어서 관리해야 합니다.
+// => 어차피 스레드당 1개 필요하므로 스레드 클래스가 관리하면 어떨까요 ?
+// => std::thread 에는 없지만 std::jthread 에는 있습니다.
 
 void foo( bool& quit_signal )
 {
