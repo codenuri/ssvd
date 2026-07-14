090301f9ca7cec9a04f0e8d5ca379bd040970d2f
ecourse <smkang@codenuri.co.kr>
2026-07-14 17:32:07 +0900

    Add files via upload

--- DAY1/09_call_once2-4.cpp
+++ DAY1/09_call_once2-4.cpp
@@ -21,6 +21,9 @@ public:
 	// => 1990년 ~ 2000년대 초반에 널리 유행한 기술.. 
 	// => 하지만 버그가 발견되어서 C++에서는 사용금지. 
 
+	// 최초 호출   : 2번의 if 문 수행 - 1번이면 되는데, 2번 하게 되므로 약간의 오버헤드
+	// 나머지 호출 : mtx.lock() 대신 if 로 조사후 반환..- case#2 보다 아주 빠르다
+
     static Singleton* getInstance()
     {
 		if ( sinstance == nullptr )
@@ -30,6 +33,7 @@ public:
 			if ( sinstance == nullptr )
 			{
 				sinstance = new Singleton;
+
 			}
 
 			mtx.unlock();
@@ -47,3 +51,25 @@ int main()
 
 
 
+// 위 기술을 사용하면 안되는 이유
+static Singleton* getInstance()
+{
+	if ( sinstance == nullptr )
+	{
+		mtx.lock();
+
+		if ( sinstance == nullptr )
+		{
+			sinstance = new Singleton; // A
+
+			// 위 A 는 컴파일러에 의해 아래 3줄로 변경됩니다.
+			// 1. Singleton 크기 메모리 할당      : temp = malloc(sizeof(Singleton));
+			// 2. 할당된 메모리(객체)의 생성자 호출 : Singleton::Singleton();
+			// 3. 메모리 주소를 sinstance에 대입   : sintance = temp;
+
+		}
+
+		mtx.unlock();
+	}
+	return sinstance;
+}
