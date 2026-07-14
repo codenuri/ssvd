a2a7d70b39acead11eff609b606c525fbe513e18
ecourse <smkang@codenuri.co.kr>
2026-07-14 09:21:15 +0900

    Add files via upload

---
+++ 01_this_thread1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <thread>	// C++11 에서 표준에 추가된 헤더.
+
+// std::this_thread namespace 안에 있는 4개의 함수
+// 1. std::this_thread::get_id()
+// 2. std::this_thread::sleep_for()
+// 3. std::this_thread::sleep_until()
+// 4. std::this_thread::yield()			
+
+
+int main()
+{
+	// 현재 스레드 ID 얻기 
+    std::cout << std::this_thread::get_id() << std::endl;
+
+	// 스레드 ID 는 정수 타입 아닙니다.
+	// => int 타입과 호환(암시적 변환) 안됩니다
+    std::thread::id tid1 = std::this_thread::get_id();
+
+}
