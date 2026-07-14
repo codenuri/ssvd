336f937d95844f1055486d50ce430ab2b2f95fa4
ecourse <smkang@codenuri.co.kr>
2026-07-14 15:55:40 +0900

    Add files via upload

--- DAY1/07_async2.cpp
+++ DAY1/07_async2.cpp
@@ -15,7 +15,16 @@ int main()
 	// std::async() : "새로운 스레드로 실행해라" 가 아닌 "비동기로 실행해라"
 	// std::launch::async 옵션    : 새로운 스레드로 실행하라
 	// std::launch::deferred 옵션 : 지연된 실행, ft.get() 할때 실행해달라(스레드 생성안됨)
-    std::future<int> ft = std::async( std::launch::async, add, 10, 20);
+//  std::future<int> ft = std::async( std::launch::async, add, 10, 20);
+//  std::future<int> ft = std::async( std::launch::deferred, add, 10, 20);
+
+	// std::launch::async | std::launch::deferred  의미
+	// => 멀티스레드가 지원되는 OS 라면 새로운 스레드 생성해서 실행
+	// => 멀티스레드가 지원 안되는 OS 라면 지연된 실행
+  	// std::future<int> ft = std::async( std::launch::deferred | std::launch::async, add, 10, 20);
+
+	// 1번째 인자를 생략하면 "위 코드와 동일" 
+	std::future<int> ft = std::async(  add, 10, 20);
 
     std::cout << "continue main : " << std::this_thread::get_id() << std::endl;
     std::this_thread::sleep_for(2s);
@@ -24,6 +33,12 @@ int main()
 
     std::cout << "result : " << ret << std::endl;
 }
+
+
+
+
+
+
 
 
 // async(비동기) 함수 : 호출하면 "즉시 반환 하고" 실행은 다른 다양한 방법으로 ...
