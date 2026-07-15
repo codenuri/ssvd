c990d79edcccabc0a86456fee234ae4ce235519f
ecourse <smkang@codenuri.co.kr>
2026-07-15 16:36:40 +0900

    Add files via upload

--- DAY2/14_latch1.cpp
+++ DAY2/14_latch1.cpp
@@ -2,21 +2,24 @@
 #include <mutex>
 #include <thread>
 #include <latch>
+#include <chrono>
+using namespace std::literals;
 
 // latch : 카운트 기반에 간단한(가벼운) 동기화 도구, C++20 에서 추가 
 std::latch mylatch(3); // 내부적으로 3을 보관
 
 void foo(const std::string& name)
 {
+	std::this_thread::sleep_for(1s);
 	std::cout << "start  work : " << name << std::endl;
+	std::this_thread::sleep_for(2s);
 	std::cout << "finish work : " << name << std::endl;
 
 	// 업무 종료후 집에 퇴근전에 보고해야 한다
 	mylatch.count_down(); // --count
-
+	
 	std::cout << "go home     : " << name << std::endl;
 }
-
 int main()
 {
 	std::jthread t1(foo, "kim"), t2(foo, "lee"), t3(foo, "park");
