8f9b4085cee2d5dc19fe38c43038ae076ce03aae
ecourse <smkang@codenuri.co.kr>
2026-07-15 15:33:14 +0900

    Add files via upload

--- DAY2/12_thread_pool4.cpp
+++ DAY2/12_thread_pool4.cpp
@@ -17,4 +17,20 @@ int main()
 
 	// #1. 직접 호출하면 "함수 종료후" 에 결과를 얻을수 있습니다.
 	int ret1 = add(1, 2);
+
+	// #2. std::async() 를 사용하면, 다른 스레드로 실행되지만, 
+	//     "함수 종료 전" 에도 미래의 결과를 얻을수 있습니다.
+	//     의미 : 종료전에도 결과를 담는 변수를 만들수 있다는 점..
+	std::future<int> ft1 = std::async(add, 1, 2);
+
+	
+	// #3. std::packaged_task 를 사용하면 "함수 호출 전" 에도 결과를 담는 변수 생성이 가능합니다.
+	std::packaged_task<int(int, int)> task(add);
+	std::future<int> ft2 = task.get_future(); // add 를 호출(실행)한적도 없음..
+
+	std::thread t(std::ref(task), 1, 2);
+
+	int ret2 = ft2.get();
+
+	t.join();
 }  
