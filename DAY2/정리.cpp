6ffe56ac8c2d13bc22d12b04515228b9ea5e4581
ecourse <smkang@codenuri.co.kr>
2026-07-15 16:49:05 +0900

    Add files via upload

---
+++ DAY2/정리.cpp
@@ -0,0 +1,34 @@
+// 정리
+// C++ Concurrency 에서 반드시 알아야 하는 것들
+
+// 1. 스레드를 생성/관리하는 기술
+// => std::thread
+// => std::jthread
+// => std::vector<> 를 가지고 여러개 스레드 관리 기술
+// => std::thread(jthread) 는 copy 될수 없지만 move 될수 있다.
+// => 최적의 스레드 개수를 결정하는 것이 핵심
+//    (HW 상황 고려, 데이터 개수 고려)
+// => thread9-3.cpp 예제가 핵심
+
+// 2. 고수준의(high-level) 스레드 동기화 => 비교적 쉬운 내용
+// => 내부 구현을 저수준의 기술사용
+
+// => std::promise/std::future
+// => std::packaged_task
+// => std::async()
+
+
+// 3. 저수준의(low-level) 스레드 동기화 => 어렵고 복잡
+
+// => thread_local 개념 - 스레드당 한개의 static storage 개념
+// => 뮤텍스 6가지 종류
+// => lock management : lock_guard, unique_lock, shared_lock, scoped_lock
+// => std::condition_variable
+// => std::semaphore, std::latch, std::barrier => C++20 에서 추가된 간단한 도구
+// => thread pool 만들기 예제.. - "다양한 스레드 기술 + C++ 문법 활용"
+
+// 4. atomic, lock-free
+
+
+// 5. parallel vs concurrency
+// 6. async/coroutine - 스레드는 아니지만 concurrency
