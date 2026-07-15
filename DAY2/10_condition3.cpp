9b25011512b5ce931061557d81ced257ebc3eb62
ecourse <smkang@codenuri.co.kr>
2026-07-15 13:24:46 +0900

    Add files via upload

--- DAY2/10_condition3.cpp
+++ DAY2/10_condition3.cpp
@@ -11,13 +11,25 @@ std::condition_variable cv;
 
 // 이번 코드 핵심 
 // => 생산자가 먼저 도착하면 어떻게 될까 ?
+// => cv.notify_one() 하는 순간에 소비자는 아직 "cv.wait()" 를 하지 않고 있다
+// => 생산자의 신호가 사라진다 ( lost wake 현상 )
+
+// [해결책]
+// => 상태 변수를 만들고
+// => 대기 전에 상태 변수 조사
+
+bool ready_data = false;
 
 void consumer()
 {
     std::this_thread::sleep_for(10ms);
 
 	std::unique_lock<std::mutex> ul(m); 
-	cv.wait(ul);	
+
+	if ( ! data_ready )	// 데이터가 준비 되면 대기할 필요 없다
+	{
+		cv.wait(ul);	
+	}
     std::cout << "consume : " << shared_data << std::endl;
 }
 
@@ -29,6 +41,8 @@ void producer()
     shared_data = 100;
     std::cout << "produce : " << shared_data << std::endl;
 
+	data_ready = true;
+	
 	cv.notify_one(); 
 }
 
