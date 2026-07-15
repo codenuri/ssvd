7ecb485b4f25ea9a1a71a8efb4bd1c9f975309b0
ecourse <smkang@codenuri.co.kr>
2026-07-15 15:54:04 +0900

    Add files via upload

---
+++ DAY2/12_thread_pool6.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+#include <future>
+#include <functional>
+#include <memory> // 스마트 포인터
+
+using namespace std::literals;
+
+int foo(int, int);
+
+
+class ThreadPool
+{
+//	using TASK = void(*)(); 
+	using TASK = std::function<void()>;
+
+	std::queue<TASK> Q; 
+
+	std::mutex m;		
+	std::condition_variable cv;	
+
+	bool stop = false; 
+	std::vector<std::thread> v; 
+public:
+	ThreadPool(int cnt)
+	{
+		for( int i= 0; i < cnt; i++)
+		{
+			v.push_back( std::thread( &ThreadPool::pool_thread_main, this));
+		}
+	}
+
+	void pool_thread_main()
+	{
+		while (true )
+		{
+			TASK task;
+			{
+				std::unique_lock<std::mutex> ul(m);
+
+				cv.wait( ul, [this](){ return !Q.empty() || stop; });					
+				if ( stop == true && Q.empty() ) 
+					return;				
+				
+				task = Q.front();
+				Q.pop();
+			}
+
+			task(); 
+		}
+	}
+
+	std::future<int> pool_add(int(*f)(int, int), int arg1, int arg2 )
+	{
+		// #1. 인자 2개를 고정해서 std::packaged_task 객체로 생성
+		std::packaged_task<int()> task(std::bind(f, arg1, arg2));
+
+
+		auto task = new std::packaged_task<int()>((std::bind(f, arg1, arg2)));
+
+
+		auto task = std::make_shared<std::packaged_task<int()>>(std::bind(f, arg1, arg2));
+
+
+		// 실행되지 않았지만 결과를 미리 꺼내서, 반환해 줄수 있습니다.
+		std::future<int> ft = task->get_future(); 
+
+		// task 를 실행하는 람다 표현식
+		auto work = [task](){ (*task)();};
+
+		{
+			std::lock_guard<std::mutex> g(m);
+			Q.push( work );
+		}
+		cv.notify_one(); 
+
+		return ft;
+	}
+
+
+
+
+	~ThreadPool()
+	{
+		{
+			std::lock_guard<std::mutex> g(m);
+			stop = true;
+		}
+		cv.notify_all(); 
+		for( auto& t : v)
+			t.join();
+	}
+};
+
+int main()
+{
+	ThreadPool tp(4);
+	
+	std::future<int> ft = tp.pool_add(&foo, 1, 2); // 결국 이코드는
+					//    std::async(&foo, 1, 2);  // 와 거의 동일합니다.	
+	
+
+	int ret = ft.get();
+
+	std::cout << "결과 : " << ret << std::endl;
+}	
+
+
+
+int foo(int a, int b)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << "foo : " << std::this_thread::get_id() << std::endl;
+		std::this_thread::sleep_for(1s);
+	}
+
+	return a + b;
+}
