455ec5c5bd29bfa7ca42cef4b9ebab26cea25cd9
ecourse <smkang@codenuri.co.kr>
2026-07-15 11:21:49 +0900

    Add files via upload

--- DAY2/08_shared_lock.cpp
+++ DAY2/08_shared_lock.cpp
@@ -7,6 +7,9 @@
 #include <string_view>
 using namespace std::literals;
 
+// std::lock_guard  : 생성자에서 lock(), 소멸자에서 unlock()
+// std::shared_lock : 생성자에서 lock_shared(), 소멸자에서 unlock_shared()
+
 std::shared_mutex m; 
 int share_data = 0;
 
@@ -14,26 +17,35 @@ void Writer()
 {
     while (1)
     {
-        m.lock();	
-		//----------------------------------
-        share_data = share_data + 1;
-        std::cout << "Writer : " << share_data << std::endl;
-        std::this_thread::sleep_for(1s);
-		//----------------------------------
-        m.unlock();
+		{
+			// std::lock_guard : 생성자에서 lock(), 소멸자에서 unlock()
+			std::lock_guard<std::shared_mutex> g(m);
+			// m.lock();				
+			share_data = share_data + 1;
+			std::cout << "Writer : " << share_data << std::endl;
+			std::this_thread::sleep_for(1s);			
+			// m.unlock();
+		}
+
         std::this_thread::sleep_for(10ms);
     }
 }
+
+
+
 void Reader(std::string_view name)
 {
     while (1)
     {
-        m.lock_shared();    
-		//-----------------------------------
-        std::cout << "Reader(" << name << ") : " << share_data << std::endl;
-        std::this_thread::sleep_for(500ms);
-		//-----------------------------------
-        m.unlock_shared();
+		{
+			std::shared_lock<std::shared_mutex> g(m);	
+								// 생성자에서 lock_shared(), 소멸자에서 unlock_shared()
+			// m.lock_shared();    
+			std::cout << "Reader(" << name << ") : " << share_data << std::endl;
+			std::this_thread::sleep_for(500ms);
+			// m.unlock_shared();
+		}
+
         std::this_thread::sleep_for(10ms);
     }
 }
