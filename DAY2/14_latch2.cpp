ccd63a26e9529f19685035136603d05880e62529
ecourse <smkang@codenuri.co.kr>
2026-07-15 16:40:20 +0900

    Add files via upload

--- DAY2/14_latch2.cpp
+++ DAY2/14_latch2.cpp
@@ -11,7 +11,7 @@ void foo(std::string name)
 	std::cout << "finish work : " << name << std::endl;
 
 //	sync_point.count_down();	 // --count 기능
-	sync_point.arrive_at_wait(); // --count 하고 count == 0 이 될때까지 대기 
+	sync_point.arrive_and_wait(); // --count 하고 count == 0 이 될때까지 대기 
 								 // 즉, 일찍 도착 한 스레드는 다른 스레드가 이 위치에 올때 까지 대기후
 								 // 모두 도착하면 다시 모두 실행시작
 
