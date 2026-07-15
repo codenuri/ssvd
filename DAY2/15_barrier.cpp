32a95df749d26052c10d20e28ee8794adc0e17ee
ecourse <smkang@codenuri.co.kr>
2026-07-15 16:43:12 +0900

    Add files via upload

---
+++ DAY2/15_barrier.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <mutex>
+#include <latch>   // C++20
+#include <barrier> // C++20
+#include <thread>
+
+//std::latch   sync_point{ 3 }; // 1회만 가능. 
+std::barrier sync_point{ 3 }; // 여러번 사용가능
+
+void foo(std::string name)
+{
+	std::cout << "start  work : " << name << std::endl;
+	std::cout << "finish work : " << name << std::endl;
+
+
+	sync_point.arrive_and_wait(); 
+	std::cout << "have dinner : " << name << std::endl;
+
+
+	sync_point.arrive_and_wait(); 
+	std::cout << "go home : " << name << std::endl;
+}
+
+int main()
+{
+	std::jthread t1(foo, "kim"), t2(foo, "lee"), t3(foo, "park");
+
+}
