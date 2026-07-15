90649e44d608770ed39424fe2d72509ceaac20e5
ecourse <smkang@codenuri.co.kr>
2026-07-15 14:40:58 +0900

    Add files via upload

--- DAY2/12_thread_pool2.cpp
+++ DAY2/12_thread_pool2.cpp
@@ -8,26 +8,34 @@ using namespace std::literals;
 
 void foo();
 //---------------------------------------------------
+using TASK = void(*)(); // 작업은 결국 "함수 포인터"
 
+std::queue<TASK> Q;     // 작업큐(함수 포인터를 담는 큐)
 
+std::mutex m;				// 동시접근 제어
+std::condition_variable cv; // Q 에 작업이 없으면 대기
 
+bool stop = false;  // pool 을 종료하기 위한 flag
 
-
-
-
-
-
-
+std::vector<std::thread> v; // pool 에 있는 N개의 스레드 관리
+//-----------------------------------------------------------
+// Q에 작업을 넣는 함수
+void pool_add_work(TASK task)
+{
+	{
+		std::lock_guard<std::mutex> g(m);
+		Q.push(task);
+	}
+	cv.notify_one();
+}
 
 int main()
 {
-	while (true)
-	{
-		getchar();	
-
-		std::thread t(foo);	
-		t.detach();
-	}
+	// 스레드 풀에 4개의 작업을 넣는 코드
+	Q.pool_add_work(foo);
+	Q.pool_add_work(foo);
+	Q.pool_add_work(foo);
+	Q.pool_add_work(foo);
 }
 
 void foo()
