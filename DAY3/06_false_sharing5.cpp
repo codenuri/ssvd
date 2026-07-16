c5c31fa156fddeeb8bf1a97694d9706f1fcff75d
ecourse <smkang@codenuri.co.kr>
2026-07-16 13:29:11 +0900

    Add files via upload

--- DAY3/06_false_sharing5.cpp
+++ DAY3/06_false_sharing5.cpp
@@ -3,6 +3,9 @@
 #include "chronometry.h"
 constexpr int sz = 10'000;
 
+// 핵심 
+// => dereferencing(*p) 는 결국 주소를 따라가서 메모리 접근하는것
+// => 루프안에서는 하지 마세요
 void f1(int* p)
 {
 	for (int i = 0; i < sz; i++)
@@ -11,10 +14,21 @@ void f1(int* p)
 	}
 	std::cout << "result: " << *p << std::endl;
 }
-
+void f2(int* p)
+{
+	int local = *p;
+	for (int i = 0; i < sz; i++)
+	{
+		local += i;
+	}
+	*p = local;
+	std::cout << "result: " << local << std::endl;
+}
 
 int main()
 {
-	chronometry(f1);
-//	chronometry(f2);
+	int k = 0;
+	chronometry(f1, &k);
+	k = 0;
+	chronometry(f2, &k);
 }
