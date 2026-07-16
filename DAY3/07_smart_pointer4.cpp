bbb33d7924e1c68d35a8cb7b2a5014ee7777c535
ecourse <smkang@codenuri.co.kr>
2026-07-16 14:14:55 +0900

    Add files via upload

--- DAY3/07_smart_pointer4.cpp
+++ DAY3/07_smart_pointer4.cpp
@@ -10,11 +10,15 @@ class Machine
 public:
 	Machine()  { std::cout << "Machine()\n"; }
 	~Machine() { std::cout << "~Machine()\n"; }
-	void start() { do_work(); }
+	void start() 
+	{ 
+		do_work(); 
+	}
 	void do_work()
 	{
 		std::cout << "[Machine] start work\n";
 		data = 100;
+		std::this_thread::sleep_for(3s);
 		std::cout << "[Machine] finish work\n";
 	}
 };
@@ -22,8 +26,8 @@ int main()
 {
 	{
 		auto sp = std::make_shared<Machine>();
-		sp->start();
-	}
+		sp->start();	// 동기적으로 모든 일을 마치고
+	}	// <= Machine 이 파괴되니까.. 안전하다. 
 
 	getchar(); // 종료 방지	
 }
