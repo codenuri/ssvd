e96cf74a6081936747cc073d4776ac5710b180a2
ecourse <smkang@codenuri.co.kr>
2026-07-16 17:34:40 +0900

    Add files via upload

--- DAY3/09_lambda8.cpp
+++ DAY3/09_lambda8.cpp
@@ -48,3 +48,10 @@ int main()
 	chronometry(use_function);
 	chronometry(use_lambda);
 }
+
+// 빠른 이유
+// => 인자로 cmp 전달시,    sort() 내부에서 다시 cmp 호출하게 됩니다.
+// => 인자로 lambda 전달시, sort() 내부에서 lambda 코드가 인라인 치환됩니다.
+// 단, 대부분 컴파일러는 인라인 치환 되려면 최적화 필요한 경우가 많습니다 => -O2 옵션
+
+// g++ 09_lambda8.cpp -std=c++20 -O2     로 컴파일
