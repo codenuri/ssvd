b5a523a9adf0ee4e41ee5b73edf9fdc6c63050d3
ecourse <smkang@codenuri.co.kr>
2026-07-16 16:06:32 +0900

    Add files via upload

--- DAY3/08_coroutine6.cpp
+++ DAY3/08_coroutine6.cpp
@@ -4,17 +4,6 @@
 #include <generator> 
 #include <vector>
 
-std::generator<int> foo()
-{
-    std::cout << "foo #1\n";
-    co_yield 1;			
-
-    std::cout << "foo #2\n";
-    co_yield 2;
-    
-    std::cout << "foo #3\n";
-}  
-
 int main()
 {
 	std::vector<int> v = {1, 2, 3, 4, 5};
@@ -23,7 +12,20 @@ int main()
 	{
 		std::cout << e << '\n';
 	}
+	//-------------------------------------------------
 	// 위 for 문의 원리를 생각해 봅시다.
+	// 위 코드를 컴파일러가 아래 처럼 변경합니다
+	auto&& cont = v;
+	auto first = cont.begin();  // v가 배열이면 "auto first = std::begin(cont)"
+	auto last  = cont.end();
+
+	for( ; first != last; ++first ) // legacy for 
+	{	
+		auto e = *first;
+		//-------------------------
+		std::cout << e << '\n';
+	}
+	//-------------------------------------------------	
 }
 
 
