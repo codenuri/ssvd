b06e80316e5a55f993c4dd2639f42009feb89405
ecourse <smkang@codenuri.co.kr>
2026-07-16 15:04:26 +0900

    Add files via upload

---
+++ DAY3/08_coroutine1.cpp
@@ -0,0 +1,9 @@
+// parallel    : 동일 시간에 여러 곳이 동시에 실행되는 것 ( multi-core CPU )
+// concurrency : 일정 시간 동안에 여러 곳이 실행될수 있는 것
+
+// concurrency
+// 1. parallel
+// 2. CPU 가 1개라도 여러곳을 번갈아 가며 실행 가능 - context switch
+// 3. 비동기 함수 
+// 4. coroutine
+// 5. ...
