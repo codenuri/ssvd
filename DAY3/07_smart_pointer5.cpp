c24de1f3c490813e7a198e39ce5dd58efdbbe19d
ecourse <smkang@codenuri.co.kr>
2026-07-16 14:21:44 +0900

    Add files via upload

--- DAY3/07_smart_pointer5.cpp
+++ DAY3/07_smart_pointer5.cpp
@@ -17,14 +17,19 @@ public:
 		std::thread t(&Machine::do_work, this); // this.do_work() 를 스레드로 실행
 		t.detach();
 	}
+	// 아래 작업은 새로운 스레드에 수행됩니다.
+	// => 작업중에는 절때 Machine 객체 파괴 되면 안됩니다.
+	// => 그런데, Machine 객체의 수명 관리는 "주스레드가 가진 sp" 입니다.
+	// => 즉, A 스레드가 사용하는 객체의 수명을 "주(다른) 스레드가 수명 관리" - 너무 위험 합니다.
 	void do_work()
-	{
+	{		
 		std::cout << "[Machine] start work\n";
 		data = 100;
 		std::this_thread::sleep_for(3s);
 		std::cout << "[Machine] finish work\n";
 	}
 };
+
 int main()
 {
 	{
