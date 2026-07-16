0659fa8948400d331aecc1a04c12ec4b48f4e7ba
ecourse <smkang@codenuri.co.kr>
2026-07-16 13:40:30 +0900

    Add files via upload

--- DAY3/07_smart_pointer2.cpp
+++ DAY3/07_smart_pointer2.cpp
@@ -23,6 +23,13 @@ int main()
 	std::shared_ptr<Machine> sp1(new Machine);
 	std::shared_ptr<Machine> sp2 = sp1;
 
+	// #1. std::shared_ptr 은 보통 raw pointer 2개로 구현
+	std::cout << sizeof(sp1) << '\n'; // sizeof(pointer*) * 2 정도. 다를수 있다. 
+
+	// #2. control block 내의 참조계수 출력
+	std::cout << sp1.use_count() << '\n';
+
 	
+
 
 }
