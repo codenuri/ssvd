5f900e03ac317be7427a07d6af9ee1e17f7c58b4
ecourse <smkang@codenuri.co.kr>
2026-07-16 16:08:46 +0900

    Add files via upload

---
+++ DAY3/08_coroutine7.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <coroutine>
+#include <utility>
+#include <generator> 
+
+std::generator<int> foo()
+{
+    std::cout << "foo #1\n";
+    co_yield 1;			
+
+    std::cout << "foo #2\n";
+    co_yield 2;
+    
+    std::cout << "foo #3\n";
+}  
+int main()
+{	
+	// std::generator<> 로 만든 코루틴을 사용하는 일반 적인 코딩 관례
+	// => range-for 의 컨테이너 위치에서 코루틴을 호출하세요
+	
+	// "6.cpp" 코드를 참고해서 아래 코드의 원리를 잘생각해 보세요
+
+	for( auto e : foo() )
+	{
+		std::cout << e << '\n';
+	}
+
+}
