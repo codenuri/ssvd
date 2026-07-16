6a91b46ac1435d7fb932243cc6d10f33559f535f
ecourse <smkang@codenuri.co.kr>
2026-07-16 14:12:26 +0900

    Add files via upload

--- DAY3/07_smart_pointer3.cpp
+++ DAY3/07_smart_pointer3.cpp
@@ -25,8 +25,19 @@ int main()
 
 	Machine* m = new Machine;
 	std::shared_ptr<Machine> sp1(m); // ok 문제 없음
-	std::shared_ptr<Machine> sp2(m); // runtime - error. 
+//	std::shared_ptr<Machine> sp2(m); // runtime - error. 
+									 // => sp1 의 control block 과는 별도의
+									 //    새로운 control block 생성
+									 // => sp1, sp2 가 다른 control block 사용하므로 버그
 
+	std::shared_ptr<Machine> sp2(sp1); // ok
+									   // sp1 의 control block 을 공유
+
+	// 문제를 발생시킨 근본원인은 코드안에
+	// => raw pointer "m" 이 있다는 것
+
+	// 처음부터 아래 처럼했어야 한다
+	auto sp3 = std::make_shared<Machine>();
 	
 }
 
